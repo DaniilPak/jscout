36e03a9f01b1dc8188bdc404ea419751d551ddb5
DaniilPak <101089638+DaniilPak@users.noreply.github.com>
2024-11-20 00:13:42 +0500

    1

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,30 +1,28 @@
-// rollup.config.js
-
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "src/index.ts",
+  input: "src/index.ts", // Entry file for the library
   output: [
     {
-      file: "dist/index.cjs.js",
+      file: "dist/index.cjs", // CommonJS file extension updated to .cjs
       format: "cjs", // CommonJS output
       exports: "auto", // Automatically determine export type
       sourcemap: true,
     },
     {
-      file: "dist/index.esm.js",
+      file: "dist/index.mjs", // ES Module file extension updated to .mjs
       format: "esm", // ES Module output
       sourcemap: true,
     },
   ],
   plugins: [
-    resolve(),
-    commonjs(),
+    resolve(), // Resolves node_modules dependencies
+    commonjs(), // Converts CommonJS to ES Modules
     typescript({
-      tsconfig: "./tsconfig.json",
-      declaration: false, // Declaration files are handled by tsc
+      tsconfig: "./tsconfig.json", // Use TypeScript configuration
+      declaration: false, // Type declarations handled separately by tsc
     }),
   ],
   external: ["tslib"], // Exclude tslib from the bundle
